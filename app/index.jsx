136ce56c81dbc1cc67131717d23fc634b970514d
pszpaidel <szpaidel.pawel@wp.pl>
2016-10-24 22:02:43 +0200

    Update

--- app/index.jsx
+++ app/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import Main from './components/main/Main';
 import config from './store/configStore';
+import './index.less';
 
 ReactDOM.render(
   <Provider store={config()}>
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,28 +1,52 @@
-const config = require('./webpack.config');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-//remove dev tools for production config
-config.devtool = null;
-
-//add optimize plugins for production
-config.plugins.push(
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new webpack.optimize.DedupePlugin(),
-  new webpack.optimize.OccurrenceOrderPlugin(),
-  new webpack.optimize.UglifyJsPlugin({
-    compressor: {
-      warnings: false,
-      screw_ie8: true
-    }
-  })
-);
-
-
-
-module.exports = config;
-
-
+module.exports = {
+  entry: './app/index.jsx',
+  output: {
+    path: './dist/js/',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devtool: null,
+  plugins: [
+    new ExtractTextPlugin("bundle.css"),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+        screw_ie8: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        }
+      },
+      {
+        test: /\.less/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!postcss-loader!less-loader")
+      },
+    ]
+  },
+  postcss: function () {
+    return [
+      require('autoprefixer')(),
+      require('cssnano')()
+    ];
+  }
+};
