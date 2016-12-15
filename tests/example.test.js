e929bccfd6bea4744fe65fecb0ff93ddfb9df7b0
pszpaidel <szpaidel.pawel@wp.pl>
2016-12-15 23:18:07 +0100

    Update

---
+++ tests/example.test.js
@@ -0,0 +1,32 @@
+var assert = require('assert');
+describe('Array', function() {
+
+  before(function() {
+    //console.log('before')
+  });
+
+  after(function() {
+    //console.log('after')
+  });
+
+  beforeEach(function() {
+    //console.log('beforeEach')
+  });
+
+  afterEach(function() {
+    //console.log('afterEach')
+  });
+
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+
+  describe('#length', function() {
+    it('should return 1 when the value has one element', function() {
+      assert.equal(1, [1].length);
+    });
+  });
+
+});
