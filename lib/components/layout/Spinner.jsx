a3511122e5ab4e4d20daafd4071c76dce3fa109c
pszpaidel <szpaidel.pawel@wp.pl>
2017-08-04 23:42:06 +0200

    Update

--- lib/components/layout/Spinner.jsx
+++ lib/components/layout/Spinner.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Spin } from 'antd';
 
-const spinner = () => <div className="spinner"><Spin size="large" /></div>;
+const spinner = () =>
+  <div className="spinner">
+    <Spin size="large" />
+  </div>;
 
 export default spinner;
